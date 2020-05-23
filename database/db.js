aa247c98e32d05b08ff48cd44e9f2a1851900738
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-23 12:58:06 +0300

    added database settings

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_HOST, DB_USER, DB_PASS, DB_PORT } = process.env;
+const { DATABASE, DB_HOST, DB_USER, DB_PASS, DB_PORT } = process.env;
 
-const sequelize = new Sequelize("online-shop(Commerce-E)", DB_USER, DB_PASS, {
+const sequelize = new Sequelize(DATABASE, DB_USER, DB_PASS, {
   host: DB_HOST,
   dialect: "postgres",
 });
