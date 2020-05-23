5a6bdbc798638b00f0855ce45cf82b2d3ef6bf2c
Gideon Mensa <gideon123ewq@gmail.com>
2020-05-23 12:07:22 +0300

    - Added sorting Route - Added filtering route - Added pagination

--- Routes/products.js
+++ Routes/products.js
@@ -4,17 +4,38 @@ const Product = require("../Modules/Products");
 const shopProdcuts = express.Router();
 shopProdcuts.use(cors());
 
-shopProdcuts.get("/", (req, res) => {
-  Product.findAll()
+const { log } = console;
+const PAGINATESKIP = "?skip=:num";
+
+shopProdcuts.get(`/${PAGINATESKIP}`, (req, res) => {
+  const { num } = req.params;
+  console.log(num);
+  Product.findAll({ limit: 2, offset: num })
     .then((product) => {
-      res.send(product);
+      res.status(200).send(product);
     })
     .catch((err) => console.log(err));
 });
 
-shopProdcuts.get("/:id", (req, res) => {
-  res.send("products...");
+// Sorting Route
+shopProdcuts.get(`/sortBy/:param`, (req, res) => {
+  const { param } = req.params;
+  log(param);
+  Product.sequelize
+    .query(`SELECT * FROM products ORDER BY products.${param}`)
+    .then((products) => res.status(200).send(products[0]))
+    .catch((err) => res.status(400).send({ error: err }));
 });
+
+shopProdcuts.get("/filterByCategory/:param", (req, res) => {
+  const { param } = req.params;
+  log(param);
+  Product.sequelize
+    .query(`SELECT * FROM products WHERE category = '${param}'`)
+    .then((products) => res.status(200).send(products[0]))
+    .catch((err) => res.status(400).send({ error: err }));
+});
+
 shopProdcuts.post("/", (req, res) => {});
 shopProdcuts.put("/:id", (req, res) => {});
 shopProdcuts.delete("/", (req, res) => {});
